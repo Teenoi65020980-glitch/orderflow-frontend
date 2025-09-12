29b91dc14e90465681de67c0cf2859765bba077a
Teenoi65020980-glitch <nantateenoi6788@gmail.com>
2025-09-12 22:05:20 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import OrderList from './components/OrderList';
+
+function App() {
+  return (
+    <div>
+      <h1>OrderFlow ระบบจัดการคำสั่งซื้อ</h1>
+      <OrderList />
+    </div>
+  );
+}
+
+export default App;
