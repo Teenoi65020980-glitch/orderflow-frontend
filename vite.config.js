c874b523754396761c33261f82d2fe1bdc78c451
Teenoi65020980-glitch <nantateenoi6788@gmail.com>
2025-09-19 18:59:08 +0700

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',       // บอกว่า index.html อยู่ที่ root
   build: {
     outDir: 'dist'
   }
